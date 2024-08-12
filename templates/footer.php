af39f50c2af2e60dbce6c1cd42de436391bb3b21
Mamun-Miah <mahadyhasen7@gmail.com>
2024-08-12 12:51:00 +0600

    v1 commit

--- templates/footer.php
+++ templates/footer.php
@@ -1,52 +1,76 @@
-<div class="container">
-  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-    <div class="col mb-3">
-      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <p class="text-body-secondary">© 2024</p>
-    </div>
+<footer class="bg-light">
+  <div class=" container">
+      <div class="row py-5 my-5 border-top row-cols-1 row-cols-sm-2 row-cols-md-4">
+        <div class="col mt-5">
+          <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+            <img src="./assets/images/logo4.png" alt="" class="img-fluid">
+          </a>
+          <div class="d-flex justify-content-center">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+            <i class="fa-brands fa-facebook fa-xl pe-2"></i></a>
 
-    <div class="col mb-3">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+            <i class="fa-brands fa-youtube fa-xl pe-2"></i></a>
 
-    </div>
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+            <i class="fa-brands fa-linkedin fa-xl pe-2"></i></a>
 
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+            <i class="fa-brands fa-square-instagram fa-xl pe-2"></i></a>
+          </div>
+        </div>
+        <!-- <div class="col mb-3">
 
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
+        </div> -->
+        <div class="col mb-3">
+          <h5 class="fw-bold mb-4 text-primary ">About Us</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2 fw-normal"><p>Maple Service Solution is on the way to the top IT consulting firms this year. We’re here to assist you in reaching your online goals with our skilled team.</p></li>
+          </ul>
+        </div>
+        <div class="col mb-3 px-5 footer-style">
+          <h5 class="fw-bold mb-4 text-primary ">Information</h5>
+          <ul class="nav flex-column fw-medium">
+            <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About us</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact us</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Terms & Condition</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div class="col mb-3">
+          <h5 class="fw-bold mb-4 text-primary">Company Info</h5>
+          <ul class="nav row">
+              <li class="">
+                <div class="d-flex ">
+                <i class="fa-solid fa-location-dot pe-2"></i>
+                <h6 class="info-title fw-bold"> Address </h6>
+                </div>
+                  
+                    <p class="info-detail"> 38(3rd floor), Shah Makhdum Avenue, Sector 13, Uttara,<br> Dhaka-1230 </p>
+              </li>
+              <li class="">
+              
+                  <div class=" d-flex">
+                  <i class="fa-solid fa-phone pe-2"></i>
+                    <h6 class="info-title fw-bold"> Phone: </h6> 
+                  </div>
+                  <p class="info-detail "> <a href="tel:Phone" class="text-dark link-underline link-underline-opacity-0 link-underline-opacity-75-hover "> +8801922557436</a></p>
+              </li>
+              <li class="">
+                  <div class=" d-flex">
+                    <i class="fa-solid fa-envelope pe-2"></i>
+                    <h6 class="info-title fw-bold"> Email: </h6> 
+                  </div> 
+                  <p class="info-detail"> <a href="mailto:info@mapleitfirm.com?subject=Contact&amp;Us" target="_blank" class="text-dark link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> info@mapleitfirm.com </a></p>
+              </li>
+            </ul>
+        </div>
+        <div class="m-auto w-100 border-top pt-3"><p class="text-center text-body-secondary fw-medium">© 2024 SkillSpark Academy</p></div>
+        
     </div>
-
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
-    <div class="m-auto w-100 border-top pt-3"><p class="text-center text-body-secondary fw-medium">© 2024 SkillSpark Academy</p></div>
-    
-  </footer>
-</div>
+  </div>
+</footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
